61e01dab87a2aea201f33e2fd3c763d8eb775af1
lxy <lxy12531@163.com>
2020-03-09 17:53:31 +0800

    Optimizing music lists

--- themes/next/source/dist/music.js
+++ themes/next/source/dist/music.js
@@ -3,11 +3,90 @@ const ap = new APlayer({
   fixed: true,
   autoplay: true,
   loop: 'all',
-  order: 'list',
+  order: 'random',
   preload: 'auto',
-  theme: '#b7daff',
+  theme: '#8B275F',
+  mutex: true,
   volume: '0.65',
   audio: [
+    {
+      name: '---------------------',
+      artist: '-------------En-----------------------------------------------',
+      url: 'https://sharefs.yun.kugou.com/202003091722/b507c6634c643b2d9137192943341dec/G119/M09/09/11/tw0DAFxuPuGAUY1MADJ89lcxGPY220.mp3',
+      cover: 'https://s2.ax1x.com/2020/03/09/8pI7En.png',
+    },
+    {
+      name: 'Lil Nas X、Billy Ray Cyrus',
+      artist: 'Old Town Road (Remix)',
+      url: 'https://sharefs.yun.kugou.com/202003091706/bef8f193dd5efc0803235bd39d7f142a/G159/M03/03/1B/f5QEAFyoRdaAJERmACZddAl9-mU591.mp3',
+      cover: 'http://singerimg.kugou.com/uploadpic/pass/softhead/100/20161011/20161011085453526.jpg'
+    },
+    {
+      name: 'Q;indivi、indivi',
+      artist: 'Lovin\'You (Minnie Riperton)',
+      url: 'https://sharefs.yun.kugou.com/202003091715/7e790a6560e7c484742d6696aed1ef55/G014/M00/08/04/Tg0DAFUJ5CiAXtQ3AFFdDn90CGM204.mp3',
+      cover: 'http://singerimg.kugou.com/uploadpic/pass/softhead/100/20150716/20150716054057985933.jpg'
+    },
+    {
+      name: 'The Chainsmokers',
+      artist: 'Paris',
+      url: 'https://sharefs.yun.kugou.com/202003091721/ecae4ff44c50c53dbf6edd3a72ee8c97/G098/M08/0E/09/AocBAFjmRAWASdX_ADYcB8dgVjQ284.mp3',
+      cover: 'https://s2.ax1x.com/2020/03/09/8pTuyF.jpg',
+    },
+    {
+      name: 'The Bosshoss',
+      artist: 'My Personal Song',
+      url: 'https://sharefs.yun.kugou.com/202003091711/8fd888f3925472d5e7b9c4478c324ef7/G029/M0A/1D/11/_ZMEAFWfasmAQhROADbwqFiJ6BM639.mp3',
+      cover: 'https://s2.ax1x.com/2020/03/09/8pTuyF.jpg',
+    },
+    {
+      name: '----------------------',
+      artist: '------------治愈---------------------------------------------',
+      url: 'https://sharefs.yun.kugou.com/202003091737/db1d68551395fe6e94604344bedff72d/G137/M01/00/1D/aZQEAFuIGfqAY1FYADcF0ktlfoY024.mp3',
+      cover: 'https://s2.ax1x.com/2020/03/09/8pWqVx.png',
+    },
+    {
+      name: '邱有句',
+      artist: '夏天的秘密',
+      url: 'http://fdfs.xmcdn.com/group20/M07/24/44/wKgJLFs2-_OCFQYuABlJQt4DYWc894.mp3',
+      cover: 'https://s2.ax1x.com/2020/03/09/8pTuyF.jpg',
+    },
+    {
+      name: '邱有句',
+      artist: '夏鸣',
+      url: 'https://sharefs.yun.kugou.com/202003091633/fb4329cac26579677ca67f8c0d042799/G095/M0A/1A/18/P5QEAFkEtTuAeORiAC7ms1v1ag8986.mp3',
+      cover: 'http://y.gtimg.cn/music/photo_new/T002R300x300M000000NzRd63756Gg.jpg',
+    },
+    {
+      name: '邱有句',
+      artist: '如果当初和你在一起',
+      url: 'http://antiserver.kuwo.cn/anti.s?useless=/resource/&format=mp3&rid=MUSIC_65981569&response=res&type=convert_url&',
+      cover: 'http://singerimg.kugou.com/uploadpic/pass/softhead/100/20190321/20190321235826969226.jpg'
+    },
+    {
+      name: 'July',
+      artist: 'My Soul',
+      url: 'https://sharefs.yun.kugou.com/202003091644/c87f35276858690e8ab9fe87fc76a9ad/G007/M01/1F/03/Rw0DAFS4KQWATVY6ADg75kkDNnE499.mp3',
+      cover: 'http://singerimg.kugou.com/uploadpic/pass/softhead/100/20160905/20160905104458149.jpg',
+    },
+    {
+      name: '龙舟',
+      artist: '天之痕',
+      url: 'https://sharefs.yun.kugou.com/202003091659/5d9ae893e11898f9f715200ff3ed29c2/G058/M05/12/13/GpQEAFdoMFCAW5fIAD3m2o51nlE762.mp3',
+      cover: 'http://singerimg.kugou.com/uploadpic/pass/softhead/100/20191025/20191025180409352912.jpg',
+    },
+    {
+      name: '----------------------',
+      artist: '----------Miss---------------------------------------------',
+      url: 'https://sharefs.yun.kugou.com/202003091733/9e333b1cf090fbf0de4788989c0d79d6/G125/M0B/01/03/vQ0DAFsaKQeAaRa-AC8GWxC17xQ015.mp3',
+      cover: 'https://s2.ax1x.com/2020/03/09/8pcfcn.png',
+    },
+    {
+      name: '枯木逢春',
+      artist: '这一生关于你的风景',
+      url: 'https://sharefs.yun.kugou.com/202003082334/0d001148c7f00461d9bd8f4a5907760d/G160/M02/07/15/gJQEAFyd9hKAOrZgAD5Nj4OO35o583.mp3',
+      cover: 'http://singerimg.kugou.com/uploadpic/pass/softhead/100/20181207/20181207145958704.jpg',
+    },
     {
       name: '本兮',
       artist: '我不说',
@@ -27,12 +106,6 @@ const ap = new APlayer({
       cover: 'https://s2.ax1x.com/2020/03/08/3zX9ts.png',
     },
     {
-      name: '枯木逢春',
-      artist: '这一生关于你的风景',
-      url: 'https://sharefs.yun.kugou.com/202003082334/0d001148c7f00461d9bd8f4a5907760d/G160/M02/07/15/gJQEAFyd9hKAOrZgAD5Nj4OO35o583.mp3',
-      cover: 'http://singerimg.kugou.com/uploadpic/pass/softhead/100/20181207/20181207145958704.jpg'
-    },
-    {
       name: '于溪悦',
       artist: '年轮说(cover:杨丞琳)',
       url: 'http://fdfs.xmcdn.com/group63/M06/E6/B0/wKgMaF33HxCS-VjLACB0viEsh8w141.mp3',
@@ -43,6 +116,12 @@ const ap = new APlayer({
       artist: '遇见(cover:孙燕姿)',
       url: 'http://antiserver.kuwo.cn/anti.s?useless=/resource/&format=mp3&rid=MUSIC_75699037&response=res&type=convert_url&',
       cover: 'https://s2.ax1x.com/2020/03/08/3zXvgx.md.jpg'
+    },
+    {
+      name: 'Doris小武、Sis晴子',
+      artist: '认真的老去(cover:张希/曹方)',
+      url: 'https://sharefs.yun.kugou.com/202003091727/afb25e0f6fc3f9cbfdb59ca9d99ce51b/G129/M0B/09/17/wQ0DAFpJB7KAXfxdADbVuUsVzn8489.mp3',
+      cover: 'http://singerimg.kugou.com/uploadpic/pass/softhead/100/20160713/20160713151939802.jpg'
     },
     {
       name: '林俊杰',
