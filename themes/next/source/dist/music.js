d407adfea07685c4193006a837d3378cbaf23075
lxy <lxy12531@163.com>
2020-03-15 23:52:10 +0800

    modify index page details

--- themes/next/source/dist/music.js
+++ themes/next/source/dist/music.js
@@ -1,7 +1,7 @@
 const ap = new APlayer({
   container: document.getElementById('aplayer'),
   fixed: true,
-  autoplay: true,
+  autoplay: false,
   loop: 'all',
   order: 'random',
   preload: 'auto',
