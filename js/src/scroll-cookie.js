7272e4269049444598687f51a15f8598c9f37132
ClearlightY <lxy12531@163.com>
2020-12-25 19:22:23 +0800

    Site updated: 2020-12-25 19:21:57

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Sat Nov 14 2020 00:17:17 GMT+0800 (GMT+08:00)
+// build time:Fri Dec 25 2020 19:20:52 GMT+0800 (GMT+08:00)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]==o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
